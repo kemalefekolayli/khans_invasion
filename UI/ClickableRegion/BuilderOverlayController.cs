5f578f6a7c9796dc57d5ba3f01272853b71730b0
Kemal Efe Kolaylı <efe.kolayli@sabanciuniv.edu>
2025-12-31 20:48:11 +0300

    ad

---
+++ UI/ClickableRegion/BuilderOverlayController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class BuilderOverlayController : MonoBehaviour {
+    [SerializeField] private GameObject overlay_farm;
+    [SerializeField] private GameObject overlay_barrack;
+    [SerializeField] private GameObject overlay_barrack2;
+    [SerializeField] private GameObject overlay_fort;
+    [SerializeField] private GameObject overlay_fort2;
+    [SerializeField] private GameObject overlay_house;
+    [SerializeField] private GameObject overlay_house2;
+    [SerializeField] private GameObject overlay_trade;
+
+}
